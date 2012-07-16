da44ab5b010de1c997cc38f47133cd7600629f66
Joseph Lord <joseph@human-friendly.com>
2012-07-16 05:45:09 +0100

    Crashing with Voiceover on now need to confirm with Voiceover off.

---
+++ VoiceOverCrash/CrashingViewController.h
@@ -0,0 +1,13 @@
+//
+//  CrashingViewController.h
+//  VoiceOverCrash
+//
+//  Created by Joseph Lord on 16/07/2012.
+//  Copyright (c) 2012 Human Friendly Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CrashingViewController : UITableViewController
+
+@end
