8b86620e92ecc0b2e2d588831f838153a64c6bbc
Joseph Lord <joseph@human-friendly.com>
2012-07-16 06:07:44 +0100

    Code minimisation.

--- VoiceOverCrash/CrashingViewController.h
+++ VoiceOverCrash/CrashingViewController.h
@@ -1,10 +1,4 @@
-//
-//  CrashingViewController.h
-//  VoiceOverCrash
-//
-//  Created by Joseph Lord on 16/07/2012.
-//  Copyright (c) 2012 Human Friendly Ltd. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 
--- VoiceOverCrash/HFAppDelegate.h
+++ VoiceOverCrash/HFAppDelegate.h
@@ -1,10 +1,3 @@
-//
-//  HFAppDelegate.h
-//  VoiceOverCrash
-//
-//  Created by Joseph Lord on 16/07/2012.
-//  Copyright (c) 2012 Human Friendly Ltd. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
